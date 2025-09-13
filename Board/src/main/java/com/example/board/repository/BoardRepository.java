3d006273968a1fc6559ecdb98a7a94e267ddebec
김민범 <beom0109@naver.com>
2025-09-13 17:24:03 +0900

    ✨ Page 처리
    page처리를 위한 jpql 추가

--- Board/src/main/java/com/example/board/repository/BoardRepository.java
+++ Board/src/main/java/com/example/board/repository/BoardRepository.java
@@ -1,11 +1,28 @@
 package com.example.board.repository;
 
 import com.example.board.domain.Board;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 // jpa jpql로 fetch join 추가
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+	
+	// 페이징 처리
+	@Query("select b from Board b where b.user.id = :id")
+	Page<Board> findByUserId(Pageable pageable, Long id);
 
 
+	// 페이징 처리
+	@Query("select b from Board b join fetch b.user")
+	Page<Board> findAll(Pageable pageable);
+
+	@Query("select b from Board b join fetch b.user")
+	List<Board> findAllBoardList();
+
+	@Query("select b from Board b join fetch b.user u where b.id = :id")
+	Board findOneBoardById(Long id);
 }
